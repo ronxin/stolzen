b4800ce8a118fddde48b8a01fe649a0179c8e32d
alexey.s.grigoriev <alexey.s.grigoriev@c1bde552-561f-916d-8c60-df72735df38b>
2012-07-04 15:23:53 +0000

    chapter 2 section 3

---
+++ trunk/sicp/chapter2/2.3/deriv.scm
@@ -0,0 +1,75 @@
+#lang scheme
+
+(define (variable? e)
+    (symbol? e)
+)
+
+(define (same-variable? v1 v2)
+    (and (variable? v1) (variable? v2) (eq? v1 v2))
+)
+
+(define (sum? e)
+    (and (pair? e) (eq? (car e) '+))
+)
+
+; first term of addition
+(define (addend e)
+    (cadr e)
+)
+
+; second term of addition
+(define (augend e)
+    (caddr e)
+)
+
+
+(define (make-sum a1 a2)
+    (list '+ a1 a2)
+)
+
+(define (product? e)
+    (and (pair? e) (eq? (car e) '*))
+)
+
+; first term of multiplication
+(define (multiplier e)
+    (cadr e)
+)
+
+; second term of multiplication
+(define (multiplicand e)
+    (caddr e)
+)
+
+(define (make-product m1 m2)
+    (list '* m1 m2)
+)
+
+(define (deriv exp var)
+    (cond 
+        ((number? exp) 0)
+        ((variable? exp)
+            (if (same-variable? exp var) 1 0))
+        ((sum? exp) 
+            (make-sum 
+                (deriv (addend exp) var)
+                (deriv (augend exp) var))
+        )
+        ((product? exp)
+            (make-sum
+                (make-product (multiplier exp)
+                              (deriv (multiplicand exp) var))
+                (make-product (deriv (multiplier exp) var)
+                              (multiplicand exp))
+            )
+        )
+        (else 
+            (error "unknown expression tupe -- DERIV" exp))
+    )
+)
+
+
+(deriv '(+ x 3) 'x)
+(deriv '(+ x y) 'x)
+(deriv '(+ x y) 'y)
+(deriv '(* (* x y) (+ x 3)) 'x)
