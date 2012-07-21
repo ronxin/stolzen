01c6d1f5e289d5f36ebb7df98db3d822dde189c4
alexey.s.grigoriev <alexey.s.grigoriev@c1bde552-561f-916d-8c60-df72735df38b>
2012-07-21 11:22:36 +0000

    sicp 2.91

--- sicp/chapter2/2.5/generic-ops/polynomial-numbers.scm
+++ sicp/chapter2/2.5/generic-ops/polynomial-numbers.scm
@@ -145,30 +145,41 @@
         (add-poly p1 (neg-poly p2))
     )
 
-    (define (div-terms L1 L2)
-        (if (empty-termlist? L1)
-            (list (the-empty-termlist) (the-empty-termlist))
-          
+    ; 2.91
+
+    (define (div-terms L1 L2) 
+        (if (empty-termlist? L1) 
+            (list (the-empty-termlist) (the-empty-termlist)) 
+         
             (let 
-                ((t1 (first-term L1))
+                ((t1 (first-term L1)) 
                  (t2 (first-term L2)))
-             
-                (if (> (order t2) (order t1))
-                    (list (the-empty-termlist) L1)
+
+                (if (> (order t2) (order t1)) 
+                    (list (the-empty-termlist) L1) 
                     
                     (let 
-                        ((new-c (div (coeff t1) (coeff t2)))
-                         (new-o (- (order t1) (order t2))))
+                        ((new-c (div (coeff t1) (coeff t2))) 
+                         (new-o (- (order t1) (order t2))) 
+                         (new-t (make-term new-o new-c))) 
                     
                         (let 
-                            ((rest-of-result
-                                <compute rest of result recursively>))
-                            <form complete result>
+                            ((rest-of-result 
+                                (div-terms (add-poly L1 (negate (mul-poly (list new-t) L2))) L2)))
+                            (list (adjoin-term new-t (car rest-of-result)) (cadr rest-of-result))
                         )
                     )
                 )
             )
         )
+    )
+
+    (define (div-poly p1 p2) 
+        (if (same-variable? (variable p1) (variable p2)) 
+            (make-poly (variable p1) 
+                (div-terms (term-list p1) 
+                (term-list p2))) 
+            (error "not the same variable -- div-poly" (list p1 p2)))
     )
 
     ; 2.84
@@ -186,6 +197,9 @@
     
     (put 'mul '(polynomial polynomial) 
        (lambda (p1 p2) (tag (mul-poly (contents p1) (contents p2)))))
+
+    (put 'div '(polynomial polynomial) 
+       (lambda (p1 p2) (tag (div-poly (contents p1) (contents p2)))))
 
     (put 'sub '(polynomial polynomial) 
        (lambda (p1 p2) (tag (sub-poly (contents p1) (contents p2)))))
