4f174dc7cfd9d57a8daa16b18cc3020d6d07d763
alexey.s.grigoriev <alexey.s.grigoriev@c1bde552-561f-916d-8c60-df72735df38b>
2013-01-15 13:29:45 +0000

    Computing for Data Analysis week 2

---
+++ courses/coursera/Computing for Data Analysis/week2/programming assignment/getmonitor.R
@@ -0,0 +1,20 @@
+getmonitor <- function(id, directory, summarize = FALSE) {
+  ## 'id' is a vector of length 1 indicating the monitor ID
+  ## number. The user can specify 'id' as either an integer, a
+  ## character, or a numeric.
+  
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'summarize' is a logical indicating whether a summary of
+  ## the data should be printed to the console; the default is
+
+  filename = sprintf("%s/%03d.csv", directory, as.numeric(id))
+  result = read.csv(filename)
+  
+  if (summarize) {
+    print(summary(result))
+  }
+  
+  result
+}
